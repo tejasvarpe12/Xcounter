54de9447b80838d5e3fe6283a4ecac396a71381b
tejasvarpe12 <140579868+tejasvarpe12@users.noreply.github.com>
2024-04-26 00:58:46 +0530

    updating according to test cases v1

--- xcounter/src/App.js
+++ xcounter/src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-
   const [count, setCount] = useState(0);
 
   const handleIncrement = () => {
     setCount(count + 1);
-  }
+  };
 
   const handleDecrement = () => {
     setCount(count - 1);
-  }
+  };
 
   return (
     <div>
       <h1>Counter App</h1>
-      <p>Counter: {count}</p>
+      <p>Count: {count}</p>
       <button onClick={handleIncrement}>
         Increment
       </button>
